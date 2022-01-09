c8d916578b4db20a86bbb2eddf2e3942ec5b420e
adityagarde <aditya.garde96@gmail.com>
2022-01-09 19:36:25 +0530

    Async calls with Completable Future

--- src/main/java/com/learnjava/moviesapiclient/MoviesClient.java
+++ src/main/java/com/learnjava/moviesapiclient/MoviesClient.java
@@ -7,6 +7,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class MoviesClient {
 
@@ -22,6 +24,38 @@ public class MoviesClient {
         var reviews = invokeMovieReviewService(movieInfoId);
 
         return new Movie(movieInfo, reviews);
+    }
+
+    public List<Movie> retrieveMovieList(List<Long> movieInfoIds) {
+
+        return movieInfoIds.stream()
+                .map(item -> retrieveMovie(item))
+                .collect(Collectors.toList());
+    }
+
+    public CompletableFuture<Movie> retrieveMovieWithCompletableFuture(Long movieInfoId) {
+
+        var movieInfo = CompletableFuture.supplyAsync(() -> invokeMovieInfoService(movieInfoId));
+        var reviews = CompletableFuture.supplyAsync(() -> invokeMovieReviewService(movieInfoId));
+
+        return movieInfo
+                .thenCombine(reviews, (movieInfo1, reviews1) -> {
+                    return new Movie(movieInfo1, reviews1);
+                });
+    }
+
+    public List<Movie> retrieveMovieListWithCompletableFuture(List<Long> movieInfoIds) {
+
+        var movieFutures = movieInfoIds
+                .stream()
+                .map(item -> retrieveMovieWithCompletableFuture(item))
+                .collect(Collectors.toList());
+
+        return movieFutures
+                .stream()
+                .map(item -> item.join())
+                .collect(Collectors.toList());
+
     }
 
     private MovieInfo invokeMovieInfoService(Long movieInfoId) {
--- src/test/java/com/learnjava/moviesapiclient/MoviesClientTest.java
+++ src/test/java/com/learnjava/moviesapiclient/MoviesClientTest.java
@@ -1,11 +1,20 @@
 package com.learnjava.moviesapiclient;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
+
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoviesClientTest {
+
+    /*
+     * We see significantly better performance when using completable future. This more profoundly seen
+     * when we returned multiple movie objects (the `list` functions).
+     */
 
     WebClient webClient = WebClient.builder()
             .baseUrl("http://localhost:8080/movies/")
@@ -13,15 +22,53 @@ class MoviesClientTest {
 
     MoviesClient moviesClient = new MoviesClient(webClient);
 
-    @Test
+    @RepeatedTest(10)
     void retrieveMovie() {
-
+        startTimer();
         var movie = moviesClient.retrieveMovie(1L);
+        timeTaken();
 
-        System.out.println("Movie Details == " + movie.toString());
+        // System.out.println("Movie Details == " + movie.toString());
 
         assert movie != null;
         assertEquals("Batman Begins", movie.getMovieInfo().getName());
         assert movie.getReviewList().size() == 1;
+    }
+
+    @RepeatedTest(10)
+    void retrieveMovieList() {
+        startTimer();
+        var movies = moviesClient.retrieveMovieList(List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L));
+        timeTaken();
+
+        // System.out.println("Movie Details == " + movies.toString());
+
+        assert movies != null;
+        assert movies.size() == 7;
+    }
+
+    @RepeatedTest(10)
+    void retrieveMovieWithCompletableFuture() {
+        startTimer();
+        var movie = moviesClient.retrieveMovieWithCompletableFuture(1L).join();
+        timeTaken();
+
+        // System.out.println("Movie Details == " + movie.toString());
+
+        assert movie != null;
+        assertEquals("Batman Begins", movie.getMovieInfo().getName());
+        assert movie.getReviewList().size() == 1;
+    }
+
+    @RepeatedTest(10)
+    void retrieveMovieListWithCompletableFuture() {
+        startTimer();
+        var movies = moviesClient.retrieveMovieListWithCompletableFuture(List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L));
+        timeTaken();
+
+        // System.out.println("Movie Details == " + movies.toString());
+
+        assert movies != null;
+        assert movies.size() == 7;
     }
 }
